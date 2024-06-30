94c884258557dae83be43cc9f9d18a9b6445623b
Fasil | Python/Odoo Developer <fasilwdr@hotmail.com>
2024-06-30 09:51:58 +0300

    Test build ipa

--- build_ipa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-app="Markdown"
-output="builds/ipa"
-product="Markdown"
-buildVersion="0.1.1"
-
-flet build ipa "$app" --output "$output" --product "$product" --build-version "$buildVersion"
